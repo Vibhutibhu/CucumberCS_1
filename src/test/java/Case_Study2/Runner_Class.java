163b01c5acc70e94838fa989ac96490cc0180159
PDC3A-Training.PDC3A <PDC3A-Training.PDC3A@P3B-1NRDGP2.dir.svc.accenture.com>
2020-01-29 05:52:05 -0600

    enter Plugin

--- src/test/java/Case_Study2/Runner_Class.java
+++ src/test/java/Case_Study2/Runner_Class.java
@@ -5,7 +5,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "Featuresfil", glue = { "Case_Study" },tags="@register")
+@CucumberOptions(features = "Featuresfil", glue = { "Case_Study" },
+plugin= {"pretty","json:target/cucumber.json"})
 public class Runner_Class {
 
 }
